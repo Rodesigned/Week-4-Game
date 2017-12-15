a1635e7dd0b75e6c54c96487423eec258833e597
Rodesigned <rodesigned@att.net>
2017-12-15 13:39:46 -0500

    update js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,90 +10,82 @@
 
 // Start with the document ready function so the code will load at the beginning of the game.
 // The player will be shown a random number at the start of the game.
-$(document).ready(function(){
-	var ranNum = Math.floor((Math.random() * 101) + 19);
-	//console.log(ranNum);
-	$('#ran_num').text(ranNum);
-
-	
-// Click on the crystals to create random numbers to add for a total score.
-// But first create the Global variable to store the random numbers.
-	
-var numsCreated = [];
-var total= 0;	
-var wins= 0;
-var losses = 0;
-
-//Now create the random numbers that will pass through each crystal.
-function numsPerCrystal(){
-	for (var i = 0; i < 4; i++) {
-		var nums = Math.floor((Math.random()*11)+1);
-		numsCreated.push(nums);
-	}
-	console.log(numsCreated);
-}
-
-numsPerCrystal();	
-
-//Each crystal click will generate a random number for the total
-
-$('#crystal_one').on ('click', function(){
-			total = total + numsCreated[0];
-			console.log("New total= " + total);
-			$('#score_num').text(total);	
-
-//If statement for winning and losing for each crystal
-					if (total === ranNum){
-						wins();
-					}
-					else if ( total > ranNum){
-						losses();
-					}		
-		});	
-$('#crystal_two').on ('click', function(){
-			total = total + numsCreated[1];
-			console.log("New total= " + total);
-			$('#score_num').text(total);	
-
-					if (total === ranNum){
-						wins();
-					}
-					else if ( total > ranNum){
-						losses();
-					}	
-		});	
-
-		$('#crystal_three').on ('click', function(){
-			total = total + numsCreated[2];
-			console.log("New total= " + total);
-			$('#score_num').text(total);
-
-	//sets win/lose conditions
-						if (total === ranNum){
-						wins();
-					}
-					else if ( total > ranNum){
-						losses();
-					}	
-		});	
-
-		$('#crystal_four').on ('click', function(){
-			total = total + numsCreated[3];
-			console.log("New total= " + total);
-			$('#score_num').text(total);	
-
-				
-						if (total === ranNum){
-						wins();
-					}
-					else if ( total > ranNum){
-						losses();
-					}
-		}); 	
+$(document).ready(function () {
+			var ranNum = Math.floor((Math.random() * 101) + 19);
+			//console.log(ranNum);
+			$('#ran_num').text(ranNum);
 
 
+			// Click on the crystals to create random numbers to add for a total score.
+			// But first create the Global variable to store the random numbers.
+
+			var numsCreated = [];
+			var total = 0;
+			var wins = 0;
+			var losses = 0;
+
+			//Now create the random numbers that will pass through each crystal.
+			var numsPerCrystal=function(){
+				for (var i = 0; i < 4; i++) {
+					var nums = Math.floor((Math.random() * 11) + 1);
+					numsCreated.push(nums);
+				}
+				//console.log(numsCreated);
+			};
+			numsPerCrystal();
+			
+
+			//Each crystal click will generate a random number for the total
+
+			$('#crystal_one').on('click', function () {
+				total = total + numsCreated[0];
+				console.log("New total= " + total);
+				$('#score_num').text(total);
+
+				//If statement for winning and losing for each crystal
+				if (total === ranNum) {
+					wins();
+				} else if (total > ranNum) {
+					losses();
+				}
+			});
+			$('#crystal_two').on('click', function () {
+				total = total + numsCreated[1];
+				console.log("New total= " + total);
+				$('#score_num').text(total);
+
+				if (total === ranNum) {
+					wins();
+				} else if (total > ranNum) {
+					losses();
+				}
+			});
+
+			$('#crystal_three').on('click', function () {
+				total = total + numsCreated[2];
+				console.log("New total= " + total);
+				$('#score_num').text(total);
+
+				//sets win/lose conditions
+				if (total === ranNum) {
+					wins();
+				} else if (total > ranNum) {
+					losses();
+				}
+			});
+
+			$('#crystal_four').on('click', function () {
+				total = total + numsCreated[3];
+				console.log("New total= " + total);
+				$('#score_num').text(total);
 
 
+				if (total === ranNum) {
+					wins();
+				} else if (total > ranNum) {
+					losses();
+				}
+			});
 
 });
 
