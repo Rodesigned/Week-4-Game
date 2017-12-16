d134a965938a397cd5ed20ed525ef2d006523372
Rodesigned <rodesigned@att.net>
2017-12-16 00:55:40 -0500

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,81 @@
-// There will be four crystals displayed as buttons on the page.
-// When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-// Your game will hide this amount until the player clicks a crystal.
-// The player wins if their total score matches the random number from the beginning of the game.
-// The player loses if their score goes above the random number.
-// The game restarts whenever the player wins or loses.
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values that will reset to zero.
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+var targetNumber = Math.floor((Math.random() * 101) + 19);
+
+  $("#number-to-guess").text(targetNumber);
+
+  var numberOptions = Math.floor((Math.random() * 12) + 1);
+
+  var counter = 0;
+
+  var wins = 0;
+  
+  var losses = 0;
+
+// Create multiple crystals with their own unique number value.
+// We begin by expanding our array to include four options.  
+// Next we create a for loop to create crystals for every numberOption.
+  for (var i = 0; i < 1; i++) {
+	 
+// For each iteration, we will create an imageCrystal
+    var imageCrystal1 = $("<img>");
+	var imageCrystal3 = $("<img>");
+	var imageCrystal2 = $("<img>");
+	var imageCrystal4 = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imageCrystal1.addClass("crystal-image");
+	imageCrystal3.addClass("crystal-image");
+	imageCrystal2.addClass("crystal-image");
+	imageCrystal4.addClass("crystal-image");
+	  
+    // Each imageCrystal will be given a src link to the crystal image
+    imageCrystal1.attr("src", "assets/images/crystal1.png");
+	imageCrystal3.attr("src", "assets/images/crystal3.png");
+	imageCrystal2.attr("src", "assets/images/crystal2.png");
+	imageCrystal4.attr("src", "assets/images/crystal4.png");
+	  
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    imageCrystal1.attr("data-crystalvalue", numberOptions);
+	imageCrystal3.attr("data-crystalvalue", numberOptions);
+	imageCrystal2.attr("data-crystalvalue", numberOptions);
+	imageCrystal4.attr("data-crystalvalue", numberOptions);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#crystals").append(imageCrystal1);
+	$("#crystals").append(imageCrystal3);
+	$("#crystals").append(imageCrystal2);
+	$("#crystals").append(imageCrystal4);
+  }
+
+  // click event applies to every single crystal on the page. Not just one.
 
 
-// Start with the document ready function so the code will load at the beginning of the game.
-// The player will be shown a random number at the start of the game.
-$(document).ready(function () {
-			var ranNum = Math.floor((Math.random() * 101) + 19);
-			//console.log(ranNum);
-			$('#ran_num').text(ranNum);
 
+  $(".crystal-image").on("click", function() {
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+	numberOptions = Math.floor((Math.random() * 12) + 1);
+	//$("#score_num").text(crystalValue);
+	// $("#score_num").append(crystalValue);
+	  
+	// Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    counter += numberOptions;
+   $("#score_num").text(counter);
+   
+// All of the same game win-lose logic applies. So the rest remains unchanged.
+       //alert("New score: " + counter);
 
-			// Click on the crystals to create random numbers to add for a total score.
-			// But first create the Global variable to store the random numbers.
+    if (counter === targetNumber) {
+      $('#alerted').text("You win!");
+    }
 
-			var numsCreated = [];
-			var total = 0;
-			var wins = 0;
-			var losses = 0;
-
-			//Now create the random numbers that will pass through each crystal.
-			var numsPerCrystal=function(){
-				for (var i = 0; i < 4; i++) {
-					var nums = Math.floor((Math.random() * 11) + 1);
-					numsCreated.push(nums);
-				}
-				//console.log(numsCreated);
-			};
-			numsPerCrystal();
-			
-
-			//Each crystal click will generate a random number for the total
-
-			$('#crystal_one').on('click', function () {
-				total = total + numsCreated[0];
-				console.log("New total= " + total);
-				$('#score_num').text(total);
-
-				//If statement for winning and losing for each crystal
-				if (total === ranNum) {
-					wins();
-				} else if (total > ranNum) {
-					losses();
-				}
-			});
-			$('#crystal_two').on('click', function () {
-				total = total + numsCreated[1];
-				console.log("New total= " + total);
-				$('#score_num').text(total);
-
-				if (total === ranNum) {
-					wins();
-				} else if (total > ranNum) {
-					losses();
-				}
-			});
-
-			$('#crystal_three').on('click', function () {
-				total = total + numsCreated[2];
-				console.log("New total= " + total);
-				$('#score_num').text(total);
-
-				//sets win/lose conditions
-				if (total === ranNum) {
-					wins();
-				} else if (total > ranNum) {
-					losses();
-				}
-			});
-
-			$('#crystal_four').on('click', function () {
-				total = total + numsCreated[3];
-				console.log("New total= " + total);
-				$('#score_num').text(total);
-
-
-				if (total === ranNum) {
-					wins();
-				} else if (total > ranNum) {
-					losses();
-				}
-			});
+    else if (counter >= targetNumber) {
+      $('#alerted').text("You lose!!");
+    }
 
 });
-
